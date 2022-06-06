3d95d1e2acdf80930817d54a15f3c13981661138
Dakota <prattdakota12@gmail.com>
2022-06-06 18:08:28 -0400

    updated todo for game module

--- game.js
+++ game.js
@@ -3,8 +3,11 @@ import Player from "./player";
 
 function Game(player) {
     return {
-        player: player,
-        computer: new Player,
+        startNewGame: async function(player) {
+            /* Player places all ships on gameboard -> AI board get randomly generated -> player sends attack on AI board
+            -> AI sends attack on player board -> when either gameboard ships all sunk is true the game ends
+            */
+        },
         gameOver: false,
     }
 }
