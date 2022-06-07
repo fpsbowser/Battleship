d8665d3c41d763ae7c4d6776094129a5d6ec5ed0
Dakota <prattdakota12@gmail.com>
2022-06-07 08:13:01 -0400

    added eventlistener function

--- dom.js
+++ dom.js
@@ -1,31 +1,36 @@
-import Game from "./game"
+import Game from "./game";
 
+const playerboardContainer = document.getElementById("playerBoard");
+const aiBoardContainer = document.getElementById("aiBoard");
+const startGameButton = document.getElementById("start");
 
-// const start = document.getElementById('start')
-// start.addEventListener('click', () => {
-//     console.log('start button')
-    
-// })
-
-
-// function Dom() {
-//     return {
-//         playerGameboardContainer: document.getElementById('playerBoard'),
-//         test: console.log()
-//     }
-// }
-
-// export default Dom
-
-
-const playerboardContainer = document.getElementById('playerBoard')
-const aiBoardContainer = document.getElementById('aiBoard')
-const startGameButton = document.getElementById('start')
-
-
+function giveUnitsEventListener() {
+  const units = document.querySelectorAll("#enemyUnit");
+  units.forEach((item) => {
+    item.addEventListener(
+      "click",
+      (e) => {
+        console.log(e);
+        if (e.target.className == "o") {
+          e.target.className = "x";
+        } else if (
+          e.target.className == "carrier" ||
+          e.target.className == "battleship" ||
+          e.target.className == "cruiser" ||
+          e.target.className == "submarine" ||
+          e.target.className == "destroyer"
+        ) {
+          e.target.className = `${e.target.className}x`;
+        }
+      },
+      { once: true }
+    );
+  });
+}
 
 export {
-    playerboardContainer,
-    startGameButton,
-    aiBoardContainer,
-}
+  playerboardContainer,
+  startGameButton,
+  aiBoardContainer,
+  giveUnitsEventListener,
+};
