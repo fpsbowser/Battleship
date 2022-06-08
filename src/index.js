047a4cc0134a956e4c68b321b23a698b2596ec03
Dakota <prattdakota12@gmail.com>
2022-06-08 18:26:05 -0400

    refactored

--- src/index.js
+++ src/index.js
@@ -1,60 +1,14 @@
-import Gameboard from "../gameboard";
-import Ship from "../ship";
+import Gameboard from "./gameboard";
+import Ship from "./ship";
 // import Dom from "../dom";
 // import Game from "../game";
-import * as Game from "../game"
-import Player from "../player";
-import * as Dom from "../dom";
+import * as Game from "./game";
+import Player from "./player";
+import * as Dom from "./dom";
 
-Dom.startGameButton.addEventListener('click', () => {
-
-    // Start the game loop from the Game module
-    // const playerOne = new Player()
-    // console.log(playerOne.gameboard)
-    // playerOne.gameboard.insertShip(playerOne.gameboard.carrier)
-    // playerOne.gameboard.insertShip(playerOne.gameboard.battleship)
-    // playerOne.gameboard.insertShip(playerOne.gameboard.cruiser)
-    // playerOne.gameboard.insertShip(playerOne.gameboard.submarine)
-    // playerOne.gameboard.insertShip(playerOne.gameboard.destroyer)
-    // playerOne.gameboard.render(Dom.playerboardContainer, 'unit')
-
-    // const ai = new Player()
-    // console.log(ai.gameboard)
-    // ai.gameboard.insertShip(ai.gameboard.carrier)
-    // ai.gameboard.insertShip(ai.gameboard.battleship)
-    // ai.gameboard.insertShip(ai.gameboard.cruiser)
-    // ai.gameboard.insertShip(ai.gameboard.submarine)
-    // ai.gameboard.insertShip(ai.gameboard.destroyer)
-    // ai.gameboard.render(Dom.aiBoardContainer, 'enemyUnit')
-
-    // Dom.giveUnitsEventListener()
-    Game.game()
-})
+// Dom.startGameButton.addEventListener('click', () => {
+//     Game.startGame()
+// })
 
 // Dom.giveUnitsEventListener()
 // console.log(Dom.units)
-
-
-// const start = document.getElementById('start')
-// start.addEventListener('click', () => {
-//     console.log('start button')
-//     console.log(playerOne.gameboard.render())
-// })
-
-
-
-
-// const shipOne = Ship(4)
-
-// console.log(shipOne.hits)
-// shipOne.hit(2)
-// console.log(shipOne.hits)
-// shipOne.hit(3)
-// console.log(shipOne.hits)
-// shipOne.hit(1)
-// console.log(shipOne.hits)
-
-// shipOne.hit(0)
-// console.log(`Ship one hits array: ${shipOne.hits}`)
-// console.log(`Ship one length array: ${shipOne.length}`)
-// console.log(`Ship one is sunk? : ${shipOne.isSunk()}`)
