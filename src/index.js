007587a1e0f9a5e930971db3d9376aa532d52bf9
Dakota <36649413+fpsbowser@users.noreply.github.com>
2022-06-10 15:04:48 -0400

    refactored

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import Gameboard from "./gameboard";
 import Ship from "./ship";
-// import Dom from "../dom";
-// import Game from "../game";
 import * as Game from "./game";
 import Player from "./player";
 import * as Dom from "./dom";
+import { startMenu } from "./setup";
 
-// Dom.startGameButton.addEventListener('click', () => {
-//     Game.startGame()
-// })
-
-// Dom.giveUnitsEventListener()
-// console.log(Dom.units)
+startMenu();
